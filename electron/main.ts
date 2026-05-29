9a7e6b5d92a40c139db6bab43c9f4a8ff9da68d5
Jay <aldriahnjay@LAPTOP-THJJ1QQV.>
2026-05-29 12:24:22 +0400

    initial commit

---
+++ electron/main.ts
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+let win: BrowserWindow;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  // DEV mode
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
