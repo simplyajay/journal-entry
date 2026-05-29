e6eb32539d2889734a3e4e0e69b5b8341a146416
Jay <aldriahnjay@LAPTOP-THJJ1QQV.>
2026-05-29 15:48:04 +0400

    added minimize, close, and toggleMaximize function

--- electron/main.ts
+++ electron/main.ts
@@ -1,12 +1,15 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain, Menu } from "electron";
 import path from "path";
 
 let win: BrowserWindow;
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 1000,
-    height: 700,
+    width: 1200,
+    height: 800,
+    titleBarStyle: "hidden",
+    transparent: true,
+
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
@@ -14,11 +17,29 @@ function createWindow() {
     },
   });
 
-  // DEV mode
+  Menu.setApplicationMenu(null);
   win.loadURL("http://localhost:5173");
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  ipcMain.on("minimize", () => {
+    win.minimize();
+  });
+
+  ipcMain.on("toggle-maximize", () => {
+    if (win.isMaximized()) {
+      win.unmaximize();
+    } else {
+      win.maximize();
+    }
+  });
+
+  ipcMain.on("close", () => {
+    win.close();
+  });
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
--- electron/preload.ts
+++ electron/preload.ts
@@ -1,5 +1,11 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("api", {
   ping: () => "pong",
+});
+
+contextBridge.exposeInMainWorld("electron", {
+  minimize: () => ipcRenderer.send("minimize"),
+  toggleMaximize: () => ipcRenderer.send("toggle-maximize"),
+  close: () => ipcRenderer.send("close"),
 });
