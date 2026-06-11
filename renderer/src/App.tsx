321f85129eb03bea1a5156ec00b7ca9daae804b2
Jay <aldriahnjay@LAPTOP-THJJ1QQV.>
2026-06-11 16:06:27 +0400

    class name adjustments

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import AppWindow from "./components/AppWindow";
+import JevForm from "./components/form/jev/JevForm";
 
 function App() {
   return (
     <AppWindow>
-      <div className="h-full w-full flex flex-row items-center justify-center gap-20">
-        <button type="button" className="p-4 text-xl text-gray-200 rounded-lg border border-gray-800">
-          Select excel file to store forms
-        </button>
-        <button type="button" className="p-4 text-xl text-gray-200 rounded-lg border border-gray-800">
-          Select excel file to store forms
-        </button>
+      <div className="px-4 py-6">
+        <div className="max-w-screen-2xl mx-auto px-4 md:px-6 lg:px-8 py-2 md:py-4">
+          <JevForm />
+        </div>
       </div>
     </AppWindow>
   );
