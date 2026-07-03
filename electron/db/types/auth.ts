ae260e7ec36147d53c259fa4c5dce9c00d8c2c6a
Aldriahn Jay <aldriahnjayjamio@gmail.com>
2026-07-03 19:11:28 +0400

    Removed user and user dtos

--- electron/db/types/auth.ts
+++ electron/db/types/auth.ts
@@ -1,18 +1,3 @@
-export type User = {
-  id: string;
-  userName: string;
-  firstName: string;
-  middleName?: string;
-  lastName: string;
-  position: string;
-  password: string;
-  createdAt: Date;
-};
-
-export type CreateUserDTO = Omit<User, "id" | "createdAt">;
-
-export type UserDTO = Omit<User, "password">;
-
 export type LoginCredentials = {
   username: string;
   password: string;
