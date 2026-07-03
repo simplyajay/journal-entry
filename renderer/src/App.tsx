38866915d733fd0b3c8967dabe5ede6fc7c9c564
Aldriahn Jay <aldriahnjayjamio@gmail.com>
2026-07-03 18:45:21 +0400

    Removed auth route. Fixed other routes - protected routes are now children of main. fixed imports

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,9 +1,13 @@
 import AppWindow from "./AppWindow";
+import Redirector from "./pages/Redirector";
+import MainLayout from "./pages/MainLayout";
+import LoginPage from "./pages/LoginPage";
+import SetupPage from "./pages/SetupPage";
 import JevForm from "./components/form/jev/JevForm";
+import Dashboard from "./components/dashboard/Dashboard";
+import List from "./components/list/List";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./pages/protected/AuthContext";
-import ProtectedRoute from "./pages/protected/ProtectedRoute";
-import AuthPage from "./pages/AuthPage";
+import { AuthProvider } from "./pages/AuthContext";
 
 const App = () => {
   return (
@@ -11,10 +15,15 @@ const App = () => {
       <HashRouter>
         <AuthProvider>
           <Routes>
-            <Route path="/" element={<Navigate to={"/auth"} replace />} />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route element={<ProtectedRoute />}>
-              <Route path="/jev/new" element={<JevForm />} />
+            <Route path="/" index element={<Redirector />} />
+            <Route path="/login" index element={<LoginPage />} />
+            <Route path="/create-organization" index element={<SetupPage />} />
+
+            <Route path="/main" element={<MainLayout />}>
+              <Route index element={<Navigate to="jev/create" replace />} />
+              <Route path="jev/dashboard" element={<Dashboard />} />
+              <Route path="jev/create" element={<JevForm />} />
+              <Route path="jev/list" element={<List />} />
             </Route>
           </Routes>
         </AuthProvider>
