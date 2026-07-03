ad43ce71a33f77543ec8214ddcb68208f6c1be1e
Aldriahn Jay <aldriahnjayjamio@gmail.com>
2026-07-03 20:45:57 +0400

    moved AuthContext and MainLayoutContext to contexts folder. Utilized MainProvider in MainLayout

--- renderer/src/pages/MainLayout.tsx
+++ renderer/src/pages/MainLayout.tsx
@@ -1,17 +1,23 @@
 import Sidebar2 from "@/components/common/sidebar/Sidebar";
+import SettingsDialog from "@/components/common/dialog/SettingsDialog";
 import { Outlet } from "react-router-dom";
+import { MainProvider } from "./contexts/MainLayoutContext";
 
 const MainLayout = () => {
   return (
-    <div className="flex h-full [--sidebar-width-icon:3rem] [--sidebar-width:12rem]">
-      <Sidebar2 />
+    <MainProvider>
+      <div className="flex h-full [--sidebar-width-icon:3rem] [--sidebar-width:12rem]">
+        <Sidebar2 />
 
-      <main className="flex flex-1 justify-center overflow-auto bg-white px-4 py-8">
-        <div className="h-fit w-full max-w-5xl">
-          <Outlet />
-        </div>
-      </main>
-    </div>
+        <main className="flex flex-1 justify-center overflow-auto bg-white px-4 py-8">
+          <div className="h-fit w-full max-w-5xl">
+            <Outlet />
+          </div>
+        </main>
+
+        <SettingsDialog />
+      </div>
+    </MainProvider>
   );
 };
 
--- renderer/src/pages/Redirector.tsx
+++ renderer/src/pages/Redirector.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./AuthContext";
+import { useAuth } from "./contexts/AuthContext";
 
 const ProtectedRoute = () => {
   const { currentUser, sessionLoading } = useAuth();
