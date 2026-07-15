967f658a53a3702daa0b00cd8e844325de4bd629
Aldriahn Jay <aldriahnjayjamio@gmail.com>
2026-07-15 20:01:15 +0400

    removed user:get handler. utilized new session data structure for login

--- electron/ipc/handlers/auth.ts
+++ electron/ipc/handlers/auth.ts
@@ -1,20 +1,24 @@
 import Store from "electron-store";
 import { ipcMain } from "electron";
 import { AuthStoreSchema, IpcResult } from "../types";
-import { getUserById, loginUser } from "../../db/repositories/auth";
+import { loginUser } from "../../db/repositories/auth";
 import type { UserDTO } from "../../db/types/user";
 import type { LoginCredentials } from "../../db/types/auth";
 
 export const store = new Store<AuthStoreSchema>();
 
 export const registerAuthHandlers = (): void => {
-  ipcMain.handle("user:login", (_event, data: LoginCredentials): IpcResult<UserDTO> => {
+  ipcMain.handle("login", (_event, data: LoginCredentials): IpcResult<UserDTO> => {
     try {
       const user = loginUser(data);
 
       const ONE_HOUR = 60 * 60 * 1000;
 
-      store.set("session", { userId: user.id, expiresAt: Date.now() + ONE_HOUR });
+      store.set("session", {
+        userId: user.id,
+        organizationId: user.organizationId,
+        expiresAt: Date.now() + ONE_HOUR,
+      });
 
       return { success: true, data: user };
     } catch (err) {
@@ -28,26 +32,10 @@ export const registerAuthHandlers = (): void => {
     }
   });
 
-  ipcMain.handle("user:logout", (): IpcResult<void> => {
+  ipcMain.handle("logout", (): IpcResult<void> => {
     try {
       store.delete("session");
       return { success: true, data: undefined };
-    } catch (err) {
-      return {
-        success: false,
-        error: {
-          type: "general",
-          message: err instanceof Error ? err.message : "Unexpected error",
-        },
-      };
-    }
-  });
-
-  ipcMain.handle("user:get", (_event, data: string): IpcResult<UserDTO> => {
-    try {
-      const user = getUserById(data);
-
-      return { success: true, data: user };
     } catch (err) {
       return {
         success: false,
