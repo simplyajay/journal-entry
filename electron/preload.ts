424c4f49dfc6f04e76a3a6ebfe4f3469a09565cb
Aldriahn Jay <aldriahnjayjamio@gmail.com>
2026-07-15 20:04:25 +0400

    added user handlers

--- electron/preload.ts
+++ electron/preload.ts
@@ -3,7 +3,11 @@ import type { CreateJournalEntryVoucherDTO } from "./db/types/jev";
 import type { AuthStoreSchema, IpcResult } from "./ipc/types";
 import type { LoginCredentials } from "./db/types/auth";
 import type { CreateOriganizationDTO } from "./db/types/organization";
-import type { UserDTO } from "./db/types/user";
+import type {
+  UpdateUserAccountDTO,
+  UpdateUserProfileDTO,
+  UserDTO,
+} from "./db/types/user";
 import { LoginHistoryDTO } from "./db/types/log";
 
 type WindowHandlers = {
@@ -12,30 +16,36 @@ type WindowHandlers = {
   close: () => void;
 };
 
-type JevHandlers = {
-  createJev: (data: CreateJournalEntryVoucherDTO) => Promise<IpcResult<string>>;
+type AuthHandlers = {
+  login: (data: LoginCredentials) => Promise<IpcResult<UserDTO>>;
+  logout: () => Promise<IpcResult<void>>;
 };
 
 type LogHandlers = {
   getLoginHistory: (userId: string) => Promise<IpcResult<LoginHistoryDTO[]>>;
 };
 
-type AuthHandlers = {
-  getUser: (data: string) => Promise<IpcResult<UserDTO>>;
-  login: (data: LoginCredentials) => Promise<IpcResult<UserDTO>>;
-  logout: () => Promise<IpcResult<void>>;
-};
-
 type OrganizationHandlers = {
   createOrganization: (data: CreateOriganizationDTO) => Promise<IpcResult<string>>;
+  getUser: (data: string) => Promise<IpcResult<UserDTO>>;
+};
+
+type UserHandlers = {
+  updateUserProfile: (data: UpdateUserProfileDTO) => Promise<IpcResult<UserDTO>>;
+  updateUserAccount: (data: UpdateUserAccountDTO) => Promise<IpcResult<UserDTO>>;
+};
+
+type JevHandlers = {
+  createJev: (data: CreateJournalEntryVoucherDTO) => Promise<IpcResult<string>>;
 };
 
 export type ElectronAPI = {
   window: WindowHandlers;
-  jev: JevHandlers;
   auth: AuthHandlers;
-  org: OrganizationHandlers;
   log: LogHandlers;
+  org: OrganizationHandlers;
+  user: UserHandlers;
+  jev: JevHandlers;
 
   getSession: () => Promise<IpcResult<AuthStoreSchema["session"] | null>>;
 };
@@ -47,24 +57,31 @@ contextBridge.exposeInMainWorld("api", {
     close: () => ipcRenderer.send("close"),
   },
 
-  jev: {
-    createJev: (data: CreateJournalEntryVoucherDTO) =>
-      ipcRenderer.invoke("jev:create", data),
-  },
-
   auth: {
-    getUser: (id: string) => ipcRenderer.invoke("user:get", id),
-    login: (data: LoginCredentials) => ipcRenderer.invoke("user:login", data),
-    logout: () => ipcRenderer.invoke("user:logout"),
-  },
-
-  org: {
-    createOrganization: (data: CreateOriganizationDTO) =>
-      ipcRenderer.invoke("organization:create", data),
+    login: (data: LoginCredentials) => ipcRenderer.invoke("login", data),
+    logout: () => ipcRenderer.invoke("logout"),
   },
 
   log: {
     getLoginHistory: (userId: string) => ipcRenderer.invoke("login-history:get", userId),
+  },
+
+  org: {
+    getUser: (id: string) => ipcRenderer.invoke("user:get", id),
+    createOrganization: (data: CreateOriganizationDTO) =>
+      ipcRenderer.invoke("organization:create", data),
+  },
+
+  user: {
+    updateUserProfile: (data: UpdateUserProfileDTO) =>
+      ipcRenderer.invoke("user:update-profile", data),
+    updateUserAccount: (data: UpdateUserAccountDTO) =>
+      ipcRenderer.invoke("user:update-account", data),
+  },
+
+  jev: {
+    createJev: (data: CreateJournalEntryVoucherDTO) =>
+      ipcRenderer.invoke("jev:create", data),
   },
 
   getSession: () => ipcRenderer.invoke("session:get"),
